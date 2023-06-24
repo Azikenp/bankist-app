08d72764e7d6ffb508e1d0776184b1afd5cb3516
Azikenp <azikenp26@gmail.com>
2023-06-24 19:15:33 +0100

    implementing delete account

--- script.js
+++ script.js
@@ -173,8 +173,29 @@ btnTransfer.addEventListener('click', function(e){
       //Update UI
       updateUI(currentAccount);
     }
+})
 
 
+//Close account
+btnClose.addEventListener('click', function(e){
+  e.preventDefault();
+
+  if( inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+    ){
+      const index = accounts.findIndex(
+        acc => acc.username === currentAccount.username
+        );
+        console.log(index);
+
+        //Delete
+      accounts.splice(index, 1);
+
+      //Hide UI
+      containerApp.style.opacity = 0;
+    }
+    inputCloseUsername.value = '';
+    inputClosePin.value = '';
 })
 
 /////////////////////////////////////////////////
