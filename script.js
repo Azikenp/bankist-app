bc08de8a36991fdc5ff9ba540c830683ac18740b
Azikenp <azikenp26@gmail.com>
2023-06-25 10:40:10 +0100

    new changes to movements

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const displayMovements = function (movements, sort = false){
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov.toFixed(2)} EUR</div>
     </div>
   `
 
@@ -86,26 +86,26 @@ const displayMovements = function (movements, sort = false){
 const calcDisplayBalance = function(acc){
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
   
-  labelBalance.textContent = `${acc.balance} EUR`
+  labelBalance.textContent = `${acc.balance.toFixed(2)} EUR`
 };
 
 const calcDisplaySummary = function(acc){
   const incomes = acc.movements
   .filter(mov => mov > 0)
   .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes} EUR`
+  labelSumIn.textContent = `${incomes.toFixed(2)} EUR`
 
   const out = acc.movements
   .filter(mov => mov < 0)
   .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)} EUR`
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)} EUR`
 
   const interest = acc.movements.
   filter(mov => mov > 0)
   .map(deposit => deposit * acc.interestRate / 100)
   .filter(int => int >= 1)
   .reduce((acc, int) => acc + int, 0); 
-  labelSumInterest.textContent = `${interest} EUR`
+  labelSumInterest.textContent = `${interest.toFixed(2)} EUR`
 };
 
 const createUsernames = function(accs){              
@@ -182,7 +182,7 @@ btnTransfer.addEventListener('click', function(e){
 btnLoan.addEventListener('click', function(e){
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   //NOTE:customer can only collect a loan if they have any deposit greater than 10% of the requested loan amount
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1 )){
